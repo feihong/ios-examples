dd009ff62dd88b29021b77b0929cc49144d707af
Feihong Hsu <feihong.hsu@gmail.com>
2016-01-12 20:51:55 -0600

    Adjust the content insets of the table so that the top cell doesn't overlap with status bar

--- PlaylistExample/PlaylistExample/MainController.swift
+++ PlaylistExample/PlaylistExample/MainController.swift
@@ -7,9 +7,20 @@ class MainController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        adjustTableViewContentInsets()
+        
         let query = MPMediaQuery.playlistsQuery()
         playlists = query.collections!
         printPlaylists()
+    }
+    
+    func adjustTableViewContentInsets() {
+        // Get the height of the status bar
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
