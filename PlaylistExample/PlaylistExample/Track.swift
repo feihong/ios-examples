9ab51d8a5dc008854a53518f082181e9aa192b67
Feihong Hsu <feihong.hsu@gmail.com>
2016-01-21 11:29:43 -0600

    Connected the remaining labels in the track view

--- PlaylistExample/PlaylistExample/Track.swift
+++ PlaylistExample/PlaylistExample/Track.swift
@@ -1,30 +1,61 @@
 import MediaPlayer
 
 protocol Track {
-//    var id: String {get}
     var title: String {get}
     var artist: String {get}
+    var album: String {get}
+    var genre: String {get}
+    var comments: String {get}
+    var lyrics: String {get}
 }
 
 struct TrackStub : Track {
-//    var id: String
-    var title: String
-    var artist: String
+    var title: String = "Track"
+    var artist: String = "Artist"
+    var album: String = ""
+    var genre: String = ""
+    var comments: String = ""
+    var lyrics: String = ""
+    
+    init(title: String, artist: String) {
+        self.title = title
+        self.artist = artist
+    }
 }
 
-struct MediaTrack : Track {
-    var id: String {
-        return "\(item.valueForProperty(MPMediaItemPropertyPersistentID)!)"
-    }
-    var title: String {
-        return item.valueForProperty(MPMediaItemPropertyTitle) as! String
-    }
-    var artist: String {
-        return (item.valueForProperty(MPMediaItemPropertyArtist) ?? "N/A") as! String
-    }
+class MediaItemWrapper {
     var item: MPMediaItem
     
     init(item: MPMediaItem) {
         self.item = item
+    }
+    
+    func getString(property: String) -> String {
+        let result = item.valueForProperty(property) ?? ""
+        return result as! String
+    }
+}
+
+class MediaTrack : MediaItemWrapper, Track {
+    var id: String {
+        return "\(item.valueForProperty(MPMediaItemPropertyPersistentID)!)"
+    }
+    var title: String {
+        return getString(MPMediaItemPropertyTitle)
+    }
+    var artist: String {
+        return getString(MPMediaItemPropertyArtist)
+    }
+    var album: String {
+        return getString(MPMediaItemPropertyAlbumTitle)
+    }
+    var genre: String {
+        return getString(MPMediaItemPropertyGenre)
+    }
+    var comments: String {
+        return getString(MPMediaItemPropertyComments)
+    }
+    var lyrics: String {
+        return getString(MPMediaItemPropertyLyrics)
     }
 }
--- PlaylistExample/PlaylistExample/TrackController.swift
+++ PlaylistExample/PlaylistExample/TrackController.swift
@@ -3,6 +3,10 @@ import UIKit
 class TrackController: UIViewController {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var artistLabel: UILabel!
+    @IBOutlet var albumLabel: UILabel!
+    @IBOutlet var genreLabel: UILabel!
+    @IBOutlet var commentsLabel: UILabel!
+    @IBOutlet var lyricsLabel: UILabel!
     
     var track: Track = TrackStub(title: "", artist: "")
     
@@ -11,5 +15,9 @@ class TrackController: UIViewController {
         navigationItem.title = track.title
         titleLabel.text = track.title
         artistLabel.text = track.artist
+        albumLabel.text = track.album
+        genreLabel.text = track.genre
+        commentsLabel.text = track.comments
+        lyricsLabel.text = track.lyrics
     }
 }
